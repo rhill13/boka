ac8b9c0b8da82bee6e05a1627e8946adf46ee225
Ryan Hill <ryanhill1128@gmail.com>
2020-05-15 08:59:05 -0500

    Using routers

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+// Imports
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const listRouter = require("./routes/list-routes");
+
 const PORT_NUM = 3000;
 
+// Setup
 const app = express();
 
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res, next) => {
-  res.send("<h1>Connected</h1>");
-});
+// Routes
+app.use(listRouter);
 
 app.listen(PORT_NUM, () => {
   console.log(`Listening on port ${PORT_NUM}...`);
